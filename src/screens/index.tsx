202c1971ce7d83d99ff26bb576c0ab29edaf29d4
Alisson Koerich <alisson.2023014305@alunocesul.com>
2024-09-27 22:34:27 -0300

    Create Project and Configured

---
+++ src/screens/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { Container } from "./styles";
+
+
+
+
+export function Home(){
+    return(
+
+        <Container>
+            
+            <Text>TESTE</Text>
+
+        </Container>
+
+
+    )
+}
