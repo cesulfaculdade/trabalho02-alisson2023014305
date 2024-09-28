b896ebafb40627233d9e8efa8db2d9c8866866a7
Alisson Koerich <alisson.2023014305@alunocesul.com>
2024-09-28 19:21:44 -0300

    create functions for add, remove and done product

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,11 +1,78 @@
-import { Text, TextInput, View } from "react-native";
+import { Alert, Text, TextInput, View } from "react-native";
 import { Button, Container, ContainerInput, ContainerTop, IconButton, Input } from "./styles";
 
 import { Header } from "@components/header";
 import { Product } from "@components/Product";
+import { useState } from "react";
+
+type Product = {
+    name: string;
+    done: boolean;
+  };
+
 
 
 export function Home(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [productName, setProductName] = useState("");
+  
+    const productDone = products.reduce((acc, product) => {
+      return product.done ? acc + 1 : acc;
+    }, 0);
+
+
+    function handleProductAdd() {
+        if (products.filter(({ name }) => name === productName).length >= 1) {
+            return Alert.alert(
+              "Produto já cadastrado!",
+              "Já existe um produto na lista com este nome!"
+            );
+          }
+          if (productName.length === 0) {
+            return Alert.alert("Adicione um produto válido");
+          }
+          setProducts((prevState) => [
+            ...prevState,
+            { name: productName, done: false },
+          ]);
+          setProductName("");
+        }
+
+        function handleProductRemove(name: string) {
+            Alert.alert("Remover", `Deseja remover o produto ${name}?`, [
+              {
+                text: "Sim",
+                onPress: () =>
+                  setProducts((prevState) =>
+                    prevState.filter((product) => product.name !== name)
+                  ),
+              },
+              {
+                text: "Não",
+                style: "cancel",
+              },
+            ]);}
+
+
+            function handleProductDone(name: string) {
+                setProducts((prevState) =>
+                  prevState.map((product) =>
+                    product.name === name ? { ...product, done: !product.done } : product
+                  )
+                );
+              }
+
+
+
+
+
+
+
+
+
+
+
     return(
 
     <Container>
